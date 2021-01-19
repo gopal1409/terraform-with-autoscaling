e7bb6a4abb70594dc15609f96a8eb0b8e59a79dd
John cena <cena@gmail.com>
2021-01-19 03:01:25 -0700

    test

--- autoscaling.tf
+++ autoscaling.tf
@@ -5,14 +5,7 @@ resource "aws_launch_configuration" "example-launchconfig" {
   instance_type = "t2.micro"
   key_name = aws_key_pair.mykeypair.key_name
   security_groups = [aws_security_group.myinstance.id]
-  user_data = <<-EOF
-		#! /bin/bash
-        sudo apt-get update
-		sudo apt-get install -y apache2
-		sudo systemctl start apache2
-		sudo systemctl enable apache2
-		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
-	    EOF
+  user_data = "#!/bin/bash\napt-get update\napt-get -y install net-tools nginx\nMYIP=`ifconfig | grep -E '(inet 10)|(addr:10)' | awk '{ print $2 }' | cut -d ':' -f2`\necho 'this is: '$MYIP > /var/www/html/index.html"
   lifecycle {
     create_before_destroy = true
   }
